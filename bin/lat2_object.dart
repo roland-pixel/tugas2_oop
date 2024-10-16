01cd406c041a0eb70a391102c31b0ff1d24b8266
roland-pixel <kharis7181@gmail.com>
2024-10-16 18:56:16 +0700

    membuat object

---
+++ bin/lat2_object.dart
@@ -0,0 +1,9 @@
+// Object adalah hasil instansiasi dari sebuah class
+// Untuk membuat object kita bisa menggunakan nama class lalu diikuti dengan kurung ()
+
+class Person {}
+
+void main() {
+  var person1 = Person();
+  Person person2 = Person();
+}
