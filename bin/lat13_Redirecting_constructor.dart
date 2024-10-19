246851373251f8963cbb34a4b81921db44eb437c
roland-pixel <kharis7181@gmail.com>
2024-10-19 18:46:52 +0700

    Kode : Redirecting Constructor

---
+++ bin/lat13_Redirecting_constructor.dart
@@ -0,0 +1,18 @@
+// Saat membuat Named Constructor, kita bisa memanggil Default Constructor, atau istilahnya adalah melakukan Redirecting Constructor
+// Cara membuat Redirecting Constructor adalah dengan menambahkan : (titik dua), lalu diikuti dengan memanggil this(parameter), dimana this() disini adalah dianggap mengakses Default Constructor
+// Saat membuat Redirecting Constructor, kita tidak bisa menambahkan body pada Redirecting Constructor
+
+class Person {
+  String nama = "tamu";
+  String? alamat;
+  final String negara = "indonesia";
+
+  Person(this.nama, this.alamat) {}
+  Person.withName(String nama) : this(nama, "");
+  Person.withAddress(String alamat) : this("", alamat);
+  void info() {
+    print("Nama: $nama");
+    print("Alamat: ${alamat ?? 'belum ada'}");
+    print("Negara: $negara");
+  }
+}
