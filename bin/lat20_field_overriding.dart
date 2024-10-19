9451d59dce88488e2522543ead4833909c172a5d
roland-pixel <kharis7181@gmail.com>
2024-10-19 19:32:32 +0700

    Kode : Field Overriding

---
+++ bin/lat20_field_overriding.dart
@@ -0,0 +1,14 @@
+// Field overriding adalah kemampuan mendeklarasikan ulang field di child class, yang sudah ada di parent class
+// Saat kita melakukan proses overriding tersebut, secara otomatis ketika kita membuat object dari class child, field yang di class parent tidak bisa diakses lagi
+// Saat melakukan method overriding, kita harus pastikan deklarasi field nya harus sama dengan di parent class nya
+
+class Person {
+  String nama = "Kharis";
+  void sayhello(String nama) {
+    print('halo $nama, nama ku ${this.nama}');
+  }
+}
+
+class Otherperson extends Person {
+  String nama = "other person";
+}
