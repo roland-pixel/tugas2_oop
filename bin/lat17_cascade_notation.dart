59f1ea6c62c872d9c9150b22ec1ad7fb419f228c
roland-pixel <kharis7181@gmail.com>
2024-10-19 19:09:05 +0700

    Kode : Menggunakan Cascade Notation

---
+++ bin/lat17_cascade_notation.dart
@@ -0,0 +1,23 @@
+// Cascade Notation merupakan operator yang bisa kita gunakan untuk memberikan beberapa operasi pada object yang sama
+// Ada dua jenis Cascase Notation, yaitu .. (titik titik), dan ?.. (tanda tanya titik titik)
+// .. digunakan untuk object yang tidak nullable, sedangkan ?.. digunakan untuk object yang nullable
+
+class User {
+  String? username;
+  String? nama;
+  String? email;
+  void info() {
+    print('username : $username');
+    print('nama : $nama');
+    print('email : $email');
+  }
+}
+
+void main() {
+  var user = User()
+    ..username = "Kharis"
+    ..nama = "Kharis Raihan"
+    ..email = 'kharis@gmail.com';
+
+  user.info();
+}
