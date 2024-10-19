b30a3eb1ec72c7db2d2092942298d6a6e215d023
roland-pixel <kharis7181@gmail.com>
2024-10-19 19:14:51 +0700

    Kode : Inheritance

---
+++ bin/lat18_inharitance.dart
@@ -0,0 +1,16 @@
+// Inheritance atau pewarisan adalah kemampuan untuk menurunkan sebuah class ke class lain
+// Dalam artian, kita bisa membuat class Parent dan class Child
+// Class Child, hanya bisa punya satu class Parent, namun satu class Parent bisa punya banyak class Child
+// Saat sebuah class diturunkan, maka semua field dan method yang ada di class Parent, secara otomatis akan dimiliki oleh class Child
+// Untuk melakukan pewarisan, di class child, kita harus menggunakan kata kunci extends lalu diikuti dengan nama class parent nya.
+
+import 'dart:nativewrappers/_internal/vm/lib/internal_patch.dart';
+
+class Manager {
+  String? nama;
+  void sayhello(String nama) {
+    print('halo $nama, nama ku ${this.nama}');
+  }
+}
+
+class viceprecident extends Manager {}
