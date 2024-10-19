c6bb37f6e4fd325ef116927b8fc9c53363fd63fa
roland-pixel <kharis7181@gmail.com>
2024-10-19 19:26:44 +0700

    Kode : Method Overriding

---
+++ bin/lat19.method_overriding.dart
@@ -0,0 +1,17 @@
+// Method overriding adalah kemampuan mendeklarasikan ulang method di child class, yang sudah ada di parent class
+// Saat kita melakukan proses overriding tersebut, secara otomatis ketika kita membuat object dari class child, method yang di class parent tidak bisa diakses lagi
+// Saat melakukan method overriding, kita harus pastikan deklarasi method nya harus sama dengan di parent class nya
+
+// Kode : Method Overriding
+class Manager {
+  String? nama;
+  void sayhello(String nama) {
+    print('halo $nama, nama ku ${this.nama}');
+  }
+}
+
+class viceprecident extends Manager {
+  void sayhello(String nama) {
+    print('halo $nama, nama ku ${this.nama}');
+  }
+}
