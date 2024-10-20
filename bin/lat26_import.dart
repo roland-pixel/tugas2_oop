dc81c1b7dc024a9c0d3cea7fbe23d4b3a36003ba
roland-pixel <kharis7181@gmail.com>
2024-10-20 18:10:25 +0700

    Kode : Import

--- bin/lat26_import.dart
+++ bin/lat26_import.dart
@@ -1,2 +1,11 @@
 // Import adalah kemampuan untuk menggunakan class, function atau variable yang berada di file yang berbeda
 // Import sangat cocok digunakan ketika kode program kita sudah banyak dan ditempatkan di bayak file
+
+import 'data/category.dart';
+
+void main() {
+  var category = Category('1', 'gedget');
+
+  print(category.id);
+  print(category.nama);
+}
