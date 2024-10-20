d09e908ae5605f907b796abfb48accbd4e65af70
roland-pixel <kharis7181@gmail.com>
2024-10-20 18:22:45 +0700

    Kode : Abstract Class

--- bin/lat27_abstract_class.dart
+++ bin/lat27_abstract_class.dart
@@ -2,5 +2,3 @@
 // Abstract class artinya, class tersebut tidak bisa dibuat sebagai object secara langsung, hanya bisa diturunkan
 // Untuk membuat sebuah class menjadi abstract, kita bisa menggunakan kata kunci abstract sebelum kata kunci class
 // Dengan demikian abstract class bisa kita gunakan sebagai kontrak untuk class child
-
-
