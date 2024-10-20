5c6d2770ea21969dc6c6dcc8114ef09392280de9
roland-pixel <kharis7181@gmail.com>
2024-10-20 18:08:50 +0700

    Kode : Class Category di data/category.dart

---
+++ bin/lat26_import.dart
@@ -0,0 +1,2 @@
+// Import adalah kemampuan untuk menggunakan class, function atau variable yang berada di file yang berbeda
+// Import sangat cocok digunakan ketika kode program kita sudah banyak dan ditempatkan di bayak file
