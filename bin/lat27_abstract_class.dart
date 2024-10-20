2958d6c8af0d68ecd8859826206e65e28d5f88e7
roland-pixel <kharis7181@gmail.com>
2024-10-20 18:13:45 +0700

    Kode : Abstract Class

---
+++ bin/lat27_abstract_class.dart
@@ -0,0 +1,14 @@
+// Saat kita membuat class, kita bisa menjadikan sebuah class sebagai abstract class.
+// Abstract class artinya, class tersebut tidak bisa dibuat sebagai object secara langsung, hanya bisa diturunkan
+// Untuk membuat sebuah class menjadi abstract, kita bisa menggunakan kata kunci abstract sebelum kata kunci class
+// Dengan demikian abstract class bisa kita gunakan sebagai kontrak untuk class child
+
+abstract class Location {
+  String? nama;
+}
+
+class City extends Location {
+  City(String nama) {
+    this.nama = nama;
+  }
+}
