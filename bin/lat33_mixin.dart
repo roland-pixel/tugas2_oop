71684365ce97c4c722c6aa76633b4640508b8388
roland-pixel <kharis7181@gmail.com>
2024-10-20 18:59:03 +0700

    Kode : Membuat Mixin

---
+++ bin/lat33_mixin.dart
@@ -0,0 +1,21 @@
+// Mixin merupakan reusable code yang bisa digunakan di kelas lain tanpa harus terkendala dengan pewarisan
+// Mixin mirip melakukan copy paste code di beberapa tempat, namun dengan cara yang lebih baik
+// Dengan Mixin, kita bisa membuat kode yang sama pada beberapa class
+// Satu Class bisa menambah lebih dari satu mixin, sama seperti interface
+// Untuk membuat Mixin, kita bisa menggunakan kata kunci mixin
+// Untuk menggunakan Mixin, kita bisa menggunakan kata kunci with, diikuti dengan Mixin nya
+
+// Kode : Membuat Mixin
+mixin Playable {
+  String? nama;
+  void play() {
+    print('play $nama');
+  }
+}
+
+mixin stoppable {
+  String? nama;
+  void stop() {
+    print("stop $nama");
+  }
+}
