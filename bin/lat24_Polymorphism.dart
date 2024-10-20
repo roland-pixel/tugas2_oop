67d559ef7982147580e5717c5b99eaccf577752a
roland-pixel <kharis7181@gmail.com>
2024-10-20 17:57:26 +0700

    Kode : Polymorphism

---
+++ bin/lat24_Polymorphism.dart
@@ -0,0 +1,29 @@
+// Polymorphism berasal dari bahasa Yunani yang berarti banyak bentuk.
+// Dalam OOP, Polymorphism adalah kemampuan sebuah object berubah bentuk menjadi bentuk lain
+// Polymorphism erat hubungannya dengan Inheritance
+
+class Employee {
+  String? nama;
+
+  Employee(this.nama);
+}
+
+class Manager extends Employee {
+  Manager(String nama) : super(nama);
+}
+
+class VicePrecident extends Manager {
+  VicePrecident(String nama) : super(nama);
+}
+
+// Kode : Polymorphism
+void main() {
+  Employee employee = Employee("Kharis");
+  print(employee);
+
+  employee = Manager("Kharis");
+  print(employee);
+
+  employee = VicePrecident("Kharis");
+  print(employee);
+}
