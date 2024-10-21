558236c1a76a849558f4c822395c093270325d51
roland-pixel <kharis7181@gmail.com>
2024-10-21 13:59:17 +0700

    Kode : Typedef

--- bin/data/sum.dart
+++ bin/data/sum.dart
@@ -9,3 +9,11 @@ class Sum {
   Sum(this.first, this.second);
   int call() => first + second;
 }
+
+// typedef
+// Typedef merupakan fitur dimana kita bisa membuat alias untuk tipe data lainnya
+// Ini cocok ketika misal terdapat class dengan nama yang sama, sehingga kita bisa menggunakan alias untuk nama lain
+// Atau kita bisa mempersingkat nama class yang panjang dengan alias
+// Kode : Typedef
+typedef Total = Sum;
+typedef Jumlah = Sum;
