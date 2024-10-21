bcc66a3108152e471691f946913bfaf429a06ce3
roland-pixel <kharis7181@gmail.com>
2024-10-21 08:37:48 +0700

    Kode : ToString Method

--- bin/data/product.dart
+++ bin/data/product.dart
@@ -10,4 +10,16 @@ class Product {
   int? getQuantity() {
     return _quantity;
   }
+
+  // Kode : ToString Method
+  String toString() {
+    return 'Product(id: $id, nama: $nama, quantyty: $_quantity)';
+  }
 }
+
+
+// tostring method
+// Sebelumnya kita sudah bahas bahwa parent class untuk semua class kecuali null adalah Object
+// Di dalam Object, terdapat method bernama toString(), method ini merupakan method untuk representasi String dari object
+// Contohnya, saat kita menggunakan function print(object), sebenarnya yang dipanggil adalah print(object.toString())
+// Kita bisa meng-override method toString() jika ingin mengimplementasikan representasi data String dari Class yang kita buat
