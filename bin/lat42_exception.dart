0266b540190719f9bc7ce419c1b0812f493dde54
roland-pixel <kharis7181@gmail.com>
2024-10-21 14:19:54 +0700

    Kode : Membuat Class Exception

--- bin/lat42_exception.dart
+++ bin/lat42_exception.dart
@@ -4,6 +4,16 @@
 // Untuk membuat sebuah exception, kita bisa menggunakan kata kunci throw, diikuti dengan object exception nya
 // https://api.dart.dev/stable/2.14.4/dart-core/Exception-class.html
 
+// membuat class exception
+// Selain menggunakan class Exception yang sudah tersedia, kita juga bisa membuat class Exception sendiri
+// Tidak ada kontrak dalam pembuatan class exception, kita bisa membuat class biasa
+// Namun sangat direkomendasikan jika membuat class exception baru, kita melakukan implements ke class Exception
+// Kode : Membuat Class Exception
+class ValidationException implements Exception {
+  String massage;
+  ValidationException(this.massage);
+}
+
 // Kode : Membuat Exception
 class Validation {
   static void validate(String username, String password) {
