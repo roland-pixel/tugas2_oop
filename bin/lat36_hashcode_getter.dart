befb7fd64cdaf86b03d7b66a7fd4a9ede8f9cea0
roland-pixel <kharis7181@gmail.com>
2024-10-21 09:44:10 +0700

    Kode : Override HashCode Method

---
+++ bin/lat36_hashcode_getter.dart
@@ -0,0 +1,26 @@
+// HashCode Getter adalah representasi integer object kita, mirip toString yang merupakan representasi String, hashCode adalah representasi integer
+// HashCode sangat bermanfaat untuk membuat struktur data unique seperti HashMap, HashSet, dan lain-lain, karena cukup menggunakan hashCode method untuk mendapatkan identitas unique object kita
+// Secara default hashCode akan mengembalikan nilai integer sesuai data di memory, namun kita bisa meng-override nya jika kita ingin
+
+// HashCode Getter
+// HashCode Getter adalah representasi integer object kita, mirip toString yang merupakan representasi String, hashCode adalah representasi integer
+// HashCode sangat bermanfaat untuk membuat struktur data unique seperti HashMap, HashSet, dan lain-lain, karena cukup menggunakan hashCode method untuk mendapatkan identitas unique object kita
+// Secara default hashCode akan mengembalikan nilai integer sesuai data di memory, namun kita bisa meng-override nya jika kita ingin
+
+// Kontrak HashCode Method
+// Tidak mudah meng-override method hashCode, karena ada kontraknya :
+// Sebanyak apapun hashCode dipanggil, untuk object yang sama, harus menghasilkan data integer yang sama
+// Jika ada 2 object yang sama jika dibandingkan menggunakan method equals, maka nilai hashCode nya juga harus sama
+
+// Kode : Override HashCode Method
+class Category {
+  String id = "";
+  String nama = "";
+  Category(this.id, this.nama);
+
+  int get hashCode {
+    var result = id.hashCode;
+    result += nama.hashCode;
+    return result;
+  }
+}
