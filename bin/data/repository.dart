476fb4b25fdb9e4139c1021ab88f1cbc858f30db
roland-pixel <kharis7181@gmail.com>
2024-10-21 13:50:21 +0700

    NoSuchMethod untuk Abstract Class

--- bin/data/repository.dart
+++ bin/data/repository.dart
@@ -1,7 +1,15 @@
-// Kode : Membuat NoSuchMethod
 import 'dart:nativewrappers/_internal/vm/lib/mirrors_patch.dart';
 
-class Repository {
+// NoSuchMethod untuk Abstract Class
+// Salah satu penggunaan NoSuchMethod yang biasanya digunakan adalah sebagai implementasi Abstract Class
+// Dengan menggunakan NoSuchMethod, kita tidak perlu mengimplementasikan method yang terdapat di abstract class lagi
+abstract class CategoryRepository {
+  void id(String id);
+  void nama(String nama);
+}
+
+// Kode : Membuat NoSuchMethod
+class Repository extends CategoryRepository {
   final String _nama;
   Repository(this._nama);
 
